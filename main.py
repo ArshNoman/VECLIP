6bb8bc7311aad2a25957d16dd1c9c683169df8c4
Arsh <arshnoman2270@gmail.com>
2021-01-01 10:49:32 +0800

    Added 4 functions for basic operations (+, -, x, /).

--- main.py
+++ main.py
@@ -1,1 +1,18 @@
 # Very Easy Calculator Library In Python (VECLIP)
+
+# -- Basic operations --
+
+def add(v1, v2):
+    print(v1 + v2)
+
+
+def substract(v1, v2):
+    print(v1 - v2)
+
+
+def multiply(v1, v2):
+    print(v1 * v2)
+
+
+def divide(v1, v2):
+    print(v1 / v2)  # Returns a float
