13b2172329f01caf2055244f2ec35cbda6a68b78
Arsh <arshnoman2270@gmail.com>
2021-01-01 16:30:38 +0800

    Added multiple geometry operations, more to be added in future commits.

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
 # Very Easy Calculator Library In Python (VECLIP)
 
+# -- Variables --
+
+pi = 3.14159265359
+
 # -- Basic operations --
+
 
 def add(number1, number2):
     print(number1 + number2)
@@ -23,15 +28,54 @@ def divide(number1, number2):
 def sqr(number):
     print(number * number)
 
+
 def sqrt(number):
     if number < 0:
-        raise ValueError("funtion 'sqrt' does not accept negative values")
-    print(number**0.5) # Returns a float
+        raise ValueError("function 'sqrt' does not accept negative values")
+    print(number**0.5)  # Returns a float
+
 
 def cube(number):
     print(number**3)
 
 # will add cube root
 
+# -- Geometric functions --
 
 
+def a_cube(side_length):
+    print(side_length**2)
+
+
+def p_cube(side_length):
+    print(side_length*4)
+
+
+def a_rectangle(shortside_length, longside_length):
+    print(shortside_length * longside_length)
+
+
+def p_rectangle(shortside_length, longside_length):
+    print((shortside_length*2)+(longside_length*2))
+
+
+def dgnl_square(side_length):
+    c = (side_length**2)*2
+    print(c**0.5)
+
+
+def dgnl_rectangle(shortside_length, longside_length):
+    a = shortside_length**2
+    b = longside_length**2
+    c = a + b
+    print(c**0.5)
+
+
+def a_circle(radius):
+    print((radius**2)*pi)
+
+
+def p_circle(radius):
+    print((2*pi)*radius)
+
+p_circle(5)
